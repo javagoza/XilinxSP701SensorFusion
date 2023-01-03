03aeab703a79dd01497fd9d73cb2b101df45000d
Enrique Albertos <enriquealbertos@hotmail.com>
2023-01-03 18:48:29 +0100

    Delete ff_thermal_hud/ff_env_cmpss_thrml/Debug directory

--- ff_thermal_hud/ff_env_cmpss_thrml/Debug/src/memory_config_g.d
+++
@@ -1,5 +0,0 @@
-src/memory_config_g.o src/memory_config_g.o: ../src/memory_config_g.c \
- ../src/memory_config.h \
- C:/sp701/mpcsi1218/sdk/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h
-../src/memory_config.h:
-C:/sp701/mpcsi1218/sdk/design_1_wrapper/export/design_1_wrapper/sw/design_1_wrapper/standalone_microblaze_0/bspinclude/include/xil_types.h:
--- ff_thermal_hud/ff_env_cmpss_thrml/Debug/src/mlx90640_api.d
+++
@@ -1,3 +0,0 @@
-src/mlx90640_api.o src/mlx90640_api.o: ../src/mlx90640_api.c \
- ../src/mlx90640_api.h
-../src/mlx90640_api.h:
